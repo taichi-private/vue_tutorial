b764184c77b4026815e404dcc6f246bc07942100
渡辺　太一 <taichi.watanabe@theport.jp>
2019-05-10 11:56:33 +0900

    todoリスト作成完了

--- javascripts/main.js
+++ javascripts/main.js
@@ -19,7 +19,29 @@ var todoStorage = {
 const app = new Vue({
   el: '#app',
   data: {
-    todos: []
+    todos: [],
+    options: [
+      { value: -1, label: '全て' },
+      { value: 0, label: '作業中' },
+      { value: 1, label: '完了' }
+    ],
+    // 選択している options の value を記憶するためのデータ
+    // 初期値を「-1」つまり「すべて」にする
+    current: -1
+  },
+  computed: {
+    computedTodos: function() {
+      // データ current が -1 ならすべて
+      // それ以外なら current と state が一致するものだけに絞り込む
+      return this.todos.filter(function(el) {
+        return this.current < 0 ? true : this.current === el.state
+      }, this)
+    },
+    labels() {
+      return this.options.reduce(function(a,b) {
+        return Object.assign(a, { [b.value]: b.label })
+      }, {})
+    }
   },
   methods: {
     doAdd: function(event, value) {
